1e6ac3465cd15bba392e64abedd8939977fbed33
takatoshi.tenko <chanten811@gmail.com>
2023-08-20 17:32:36 +0900

    add: hello worldの出力

--- environment/hello_app/config/routes.rb
+++ environment/hello_app/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   #get "URL" => "コントローラ名#アクション名"
-  get "momocloapp" => "top#index"
-  get "momocloapp/members" => "members#index" 
-  get "momocloapp/member/:id" => "members#show"
+  # get "momocloapp" => "top#index"
+  # get "momocloapp/members" => "members#index" 
+  # get "momocloapp/member/:id" => "members#show"
+  root "application#hello"
 end
