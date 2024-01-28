a7a117e52b5a2f69e8ca87b45bde7731f51fb3f0
takatoshi.tenko <chanten811@gmail.com>
2024-01-28 23:58:53 +0900

    Basic認証、削除機能、concern実装

--- api/app/models/article.rb
+++ api/app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ApplicationRecord
-  has_many :comments
+  include Visible
+
+  has_many :comments, dependencies: :destroy
 
   validates :title, presence: true
-  validates :body, presence: true, length: {minimum: 10}
+  validates :body, presence: true, length: { minimum: 10 }
 end
