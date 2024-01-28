9949f60e1dc4b7d27c670dacbc45c4e1b75f9d8b
takatoshi.tenko <chanten811@gmail.com>
2024-01-29 00:13:33 +0900

    misc

--- api/app/models/article.rb
+++ api/app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
   include Visible
 
-  has_many :comments, dependencies: :destroy
+  has_many :comments, dependent: :destroy
 
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
