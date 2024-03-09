f38648e04571e1e882096953e34ddaff685cd7d3
takatoshi.tenko <chanten811@gmail.com>
2024-03-09 19:54:05 +0900

    add: cookiesコントローラー追加

---
+++ api/app/controllers/cookies_controller.rb
@@ -0,0 +1,10 @@
+class CookiesController < ApplicationController
+  def set_cookie
+    cookies.encrypted[:expiration_date] = Date.tomorrow # => Thu, 20 Mar 2014
+    redirect_to action: 'read_cookie'
+  end
+
+  def read_cookie
+    cookies.encrypted[:expiration_date]
+  end
+end
